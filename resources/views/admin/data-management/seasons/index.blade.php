ee262bbd417a005efb66375e2139c91863b809e5
Giulio Troccoli <giulio@troccoli.it>
2016-04-14 18:35:45 +0100

    dev: Removed spurious modal popup for delte confirmation

--- resources/views/admin/data-management/seasons/index.blade.php
+++ resources/views/admin/data-management/seasons/index.blade.php
@@ -38,29 +38,6 @@
     </div>
 </div>
 
-<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
-            </div>
-            <div class="modal-body">
-                <p>You are about to delete <b><i class="title"></i></b> record, this procedure is irreversible.</p>
-                <p>Do you want to proceed?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-danger btn-ok">Delete</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script>
-
-</script>
-
 @endsection
 
 @section('javascript')
