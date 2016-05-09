b73fe594a8f163022b0ed323dc04593a4a479352
Giulio Troccoli <giulio@troccoli.it>
2016-05-09 23:36:20 +0100

    dev: finished test for the Available Appointments CRUD

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,8 +13,8 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name'           => $faker->name,
-        'email'          => $faker->email,
+        'name'           => $faker->unique()->name,
+        'email'          => $faker->unique()->email,
         'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
@@ -22,13 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
 $factory->define(\App\Models\Season::class, function (\Faker\Generator $faker) {
     return [
-        'season' => $faker->word,
+        'season' => $faker->unique()->word,
     ];
 });
 
 $factory->define(App\Models\Division::class, function (\Faker\Generator $faker) {
     return [
-        'division'  => $faker->word,
+        'division'  => $faker->unique()->word,
         'season_id' => function () {
             return factory(App\Models\Season::class)->create()->id;
         },
@@ -37,13 +37,13 @@ $factory->define(App\Models\Division::class, function (\Faker\Generator $faker) 
 
 $factory->define(\App\Models\Club::class, function (\Faker\Generator $faker) {
     return [
-        'club' => $faker->word,
+        'club' => $faker->unique()->word,
     ];
 });
 
 $factory->define(App\Models\Team::class, function (\Faker\Generator $faker) {
     return [
-        'team'    => $faker->word,
+        'team'    => $faker->unique()->word,
         'club_id' => function () {
             return factory(App\Models\Club::class)->create()->id;
         },
@@ -52,12 +52,44 @@ $factory->define(App\Models\Team::class, function (\Faker\Generator $faker) {
 
 $factory->define(\App\Models\Role::class, function (\Faker\Generator $faker) {
     return [
-        'role' => $faker->word,
+        'role' => $faker->unique()->word,
     ];
 });
 
 $factory->define(\App\Models\Venue::class, function (\Faker\Generator $faker) {
     return [
-        'venue' => $faker->word,
+        'venue' => $faker->unique()->word,
+    ];
+});
+
+$factory->define(\App\Models\Fixture::class, function (\Faker\Generator $faker) {
+    return [
+        'division_id'  => function () {
+            return factory(\App\Models\Division::class)->create()->id;
+        },
+        'home_team_id' => function () {
+            return factory(\App\Models\Team::class)->create()->id;
+        },
+        'away_team_id' => function () {
+            return factory(\App\Models\Team::class)->create()->id;
+        },
+        'venue_id'     => function () {
+            return factory(\App\Models\Venue::class)->create()->id;
+        },
+        'match_number' => $faker->numberBetween(1, 100),
+        'match_date'   => $faker->date(),
+        'warm_up_time' => $faker->time(),
+        'start_time'   => $faker->time(),
+    ];
+});
+
+$factory->define(\App\Models\AvailableAppointment::class, function (\Faker\Generator $faker) {
+    return [
+        'fixture_id' => function () {
+            return factory(\App\Models\Fixture::class)->create()->id;
+        },
+        'role_id'    => function () {
+            return factory(\App\Models\Role::class)->create()->id;
+        },
     ];
 });
--- tests/Admin/DataManagement/AvailableAppointmentsTableTest.php
+++ tests/Admin/DataManagement/AvailableAppointmentsTableTest.php
@@ -9,19 +9,243 @@
 namespace Admin\DataManagement;
 
 use Tests\TestCase;
+use App\Models\AvailableAppointment;
+use App\Models\Fixture;
+use App\Models\Role;
 
 class AvailableAppointmentsTableTest extends TestCase
 {
-    public function testRedirectToLoginIfNotAdmin()
+    const BASE_ROUTE = 'admin.data-management.available-appointments';
+
+    public function testRedirectIfNotAdmin()
     {
-        $this->visit(route('admin.data-management.available-appointments.index'))
+        $this->visit(route(self::BASE_ROUTE . '.index'))
             ->seePageIs(route('login'));
+
+        $this->visit(route(self::BASE_ROUTE . '.create'))
+            ->seePageIs(route('login'));
+
+        $this->visit(route(self::BASE_ROUTE . '.show', [1]))
+            ->seePageIs(route('login'));
+
+        $this->visit(route(self::BASE_ROUTE . '.edit', [1]))
+            ->seePageIs(route('login'));
+
+        $this->call('POST', route(self::BASE_ROUTE . '.store'));
+        $this->assertResponseStatus(302);
+
+        $this->call('DELETE', route(self::BASE_ROUTE . '.destroy', [1]));
+        $this->assertResponseStatus(302);
+
+        $this->call('PUT', route(self::BASE_ROUTE . '.update', [1]));
+        $this->assertResponseStatus(302);
     }
 
     public function testBreadcrumbs()
     {
         $this->be($this->getFakeUser());
-        $this->breadcrumbsTests('admin.data-management.available-appointments.index', 'Available appointments');
+        $this->breadcrumbsTests(self::BASE_ROUTE . '.index', 'Available appointments');
     }
 
+    public function testAddAvailableAppointment()
+    {
+        $this->be($this->getFakeUser());
+
+        /** @var AvailableAppointment $availableAppointment */
+        $availableAppointment = factory(AvailableAppointment::class)->create();
+        $availableAppointmentId = $availableAppointment->id;
+        $fixtureId = $availableAppointment->fixture_id;
+        $roleId = $availableAppointment->role_id;
+
+        $otherFixtureId = factory(Fixture::class)->create()->id;
+        $otherRoleId = factory(Role::class)->create()->id;
+
+        // Brand new appointment
+        $this->visit(route(self::BASE_ROUTE . '.create'))
+            ->select($otherFixtureId, 'fixture_id')
+            ->select($otherRoleId, 'role_id')
+            ->press('Add')
+            ->seePageIs(route(self::BASE_ROUTE . '.index'))
+            ->seeInElement('#flash-notification .alert.alert-success', 'Appointment added!')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId + 1,
+                'fixture_id' => $otherFixtureId,
+                'role_id'    => $otherRoleId,
+            ]);
+
+        // Already existing appointment in the same fixture with different role
+        $this->visit(route(self::BASE_ROUTE . '.create'))
+            ->select($fixtureId, 'fixture_id')
+            ->select($otherRoleId, 'role_id')
+            ->press('Add')
+            ->seePageIs(route(self::BASE_ROUTE . '.index'))
+            ->seeInElement('#flash-notification .alert.alert-success', 'Appointment added!')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId + 2,
+                'fixture_id' => $fixtureId,
+                'role_id'    => $otherRoleId,
+            ]);
+
+        // Already existing appointment in the same role with different fixture
+        $this->visit(route(self::BASE_ROUTE . '.create'))
+            ->select($otherFixtureId, 'fixture_id')
+            ->select($roleId, 'role_id')
+            ->press('Add')
+            ->seePageIs(route(self::BASE_ROUTE . '.index'))
+            ->seeInElement('#flash-notification .alert.alert-success', 'Appointment added!')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId + 3,
+                'fixture_id' => $otherFixtureId,
+                'role_id'    => $roleId,
+            ]);
+
+        // Already existing appointment
+        $this->visit(route(self::BASE_ROUTE . '.create'))
+            ->select($fixtureId, 'fixture_id')
+            ->select($roleId, 'role_id')
+            ->press('Add')
+            ->seePageIs(route(self::BASE_ROUTE . '.create'))
+            ->seeInElement('.alert.alert-danger', 'Appointment already added.')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId,
+                'fixture_id' => $fixtureId,
+                'role_id'    => $roleId,
+            ]);
+    }
+
+    public function testEditAvailableAppointment()
+    {
+        $this->be($this->getFakeUser());
+
+        /** @var AvailableAppointment $availableAppointment */
+        $availableAppointment = factory(AvailableAppointment::class)->create();
+        $availableAppointmentId = $availableAppointment->id;
+        $fixtureId = $availableAppointment->fixture_id;
+        $roleId = $availableAppointment->role_id;
+
+        $otherFixtureId = factory(Fixture::class)->create()->id;
+        $otherRoleId = factory(Role::class)->create()->id;
+
+        // Change the role of the existing appointment
+        $this->seeInDatabase('available_appointments', [
+            'id'         => $availableAppointmentId,
+            'fixture_id' => $fixtureId,
+            'role_id'    => $roleId,
+        ])
+            ->visit(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->select($otherRoleId, 'role_id')
+            ->press('Update')
+            ->seePageIs(route(self::BASE_ROUTE . '.index'))
+            ->seeInElement('#flash-notification .alert.alert-success', 'Appointment updated!')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId,
+                'fixture_id' => $fixtureId,
+                'role_id'    => $otherRoleId,
+            ]);
+        $roleId = $otherRoleId;
+
+        // Change the fixture of the existing appointment
+        $this->seeInDatabase('available_appointments', [
+            'id'         => $availableAppointmentId,
+            'fixture_id' => $fixtureId,
+            'role_id'    => $roleId,
+        ])
+            ->visit(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->select($otherFixtureId, 'fixture_id')
+            ->press('Update')
+            ->seePageIs(route(self::BASE_ROUTE . '.index'))
+            ->seeInElement('#flash-notification .alert.alert-success', 'Appointment updated!')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId,
+                'fixture_id' => $otherFixtureId,
+                'role_id'    => $roleId,
+            ]);
+        $fixtureId = $otherFixtureId;
+
+        // Already existing availableAppointment in the same fixture
+        /** @var AvailableAppointment $anotherAvailableAppointment */
+        $anotherAvailableAppointment = factory(AvailableAppointment::class)->create(['fixture_id' => $fixtureId]);
+        $anotherAvailableAppointmentRoleId = $anotherAvailableAppointment->role_id;
+
+        $this->seeInDatabase('available_appointments', [
+            'id'         => $availableAppointmentId,
+            'fixture_id' => $fixtureId,
+            'role_id'    => $roleId,
+        ])
+            ->visit(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->select($anotherAvailableAppointmentRoleId, 'role_id')
+            ->press('Update')
+            ->seePageIs(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->seeInElement('.alert.alert-danger', 'Appointment already added.')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId,
+                'fixture_id' => $fixtureId,
+                'role_id'    => $roleId,
+            ]);
+
+        // Already existing availableAppointment in the same role
+        /** @var AvailableAppointment $yetAnotherAvailableAppointment */
+        $yetAnotherAvailableAppointment = factory(AvailableAppointment::class)->create(['role_id' => $roleId]);
+        $yetAnotherAvailableAppointmentFixtureId = $yetAnotherAvailableAppointment->fixture_id;
+
+        $this->seeInDatabase('available_appointments', [
+            'id'         => $availableAppointmentId,
+            'fixture_id' => $fixtureId,
+            'role_id'    => $roleId,
+        ])
+            ->visit(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->select($yetAnotherAvailableAppointmentFixtureId, 'fixture_id')
+            ->press('Update')
+            ->seePageIs(route(self::BASE_ROUTE . '.edit', [$availableAppointmentId]))
+            ->seeInElement('.alert.alert-danger', 'Appointment already added.')
+            ->seeInDatabase('available_appointments', [
+                'id'         => $availableAppointmentId,
+                'fixture_id' => $fixtureId,
+                'role_id'    => $roleId,
+            ]);
+    }
+
+    public function testShowAvailableAppointment()
+    {
+        $this->be($this->getFakeUser());
+
+        /** @var AvailableAppointment $availableAppointment */
+        $availableAppointment = factory(AvailableAppointment::class)->create();
+        $availableAppointmentId = $availableAppointment->id;
+        $fixtureId = $availableAppointment->fixture_id;
+        $roleId = $availableAppointment->role_id;
+
+        /** @var Fixture $fixture */
+        $fixture = Fixture::find($fixtureId);
+        $fixtureName = $fixture->fixture;
+
+        /** @var Role $role */
+        $role = Role::find($roleId);
+        $roleName = $role->role;
+
+        $this->visit(route(self::BASE_ROUTE . '.show', [$availableAppointmentId]))
+            ->seeInElement('tbody tr td:nth-child(1)', $availableAppointmentId)
+            ->seeInElement('tbody tr td:nth-child(2)', $fixtureName)
+            ->seeInElement('tbody tr td:nth-child(3)', $roleName);
+    }
+
+    public function testDeleteAvailableAppointment()
+    {
+        $this->be($this->getFakeUser());
+
+        /** @var AvailableAppointment $availableAppointment */
+        $availableAppointment = factory(AvailableAppointment::class)->create();
+        $availableAppointmentId = $availableAppointment->id;
+        $fixtureId = $availableAppointment->fixture_id;
+        $roleId = $availableAppointment->role_id;
+
+        $this->seeInDatabase('available_appointments', [
+            'id'         => $availableAppointmentId,
+            'fixture_id' => $fixtureId,
+            'role_id'    => $roleId])
+            ->call('DELETE', route(self::BASE_ROUTE . '.destroy', [$availableAppointmentId]))
+            ->isRedirect(route(self::BASE_ROUTE . '.index'));
+
+        $this->dontSeeInDatabase('available_appointments', ['id' => $availableAppointmentId]);
+    }
 }
