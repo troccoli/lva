a81df42a77d470f6c724bea5fbc038501dffd58d
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2019-05-01 16:18:07 +0100

    add docker commands to run laravel dusk

---
+++ tests/Browser/PingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use LVA\User;
+use Tests\Browser\Pages\HomePage;
+use Tests\DuskTestCase;
+
+class PingTest extends DuskTestCase
+{
+    public function testPing()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->dump();
+//                ->assertSee('London Volleyball Association');
+        });
+    }
+}
