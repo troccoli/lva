98208ae0197870ac97c5cbaf557137e30bc2fffb
Giulio Troccoli-Allard <giulio@troccoli.it>
2019-05-17 19:37:40 +0100

    Add config for Travis

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -39,7 +39,6 @@ abstract class DuskTestCase extends BaseTestCase
             'window-size=1920,1080',
         ]);
 
-        var_dump(app().env());
         var_dump(config('testing'));
         if (config('testing.use_selenium')) {
             return RemoteWebDriver::create(
