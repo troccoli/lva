4dbf842ae085a6a1dd26dd32b40d3540927b7a99
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2019-05-24 12:47:10 +0100

    Refactor authenticaed routes in their own group
    The /dashboard route is the only one, so far, that needs authentication.
    It added the auth middleware in the controller, but since I will have
    a lot more routes which will need an authenticated user it's easier to
    add the middleware in the routes file rather than in each single
    controller.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,6 @@ use Illuminate\Contracts\Support\Renderable;
 
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index(): Renderable
     {
         return view('dashboard');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::middleware('auth')
+    ->group(function (): void {
+        Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    });
