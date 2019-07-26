1186bef557cbacc35a32aaac150c9def24219574
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2019-07-26 16:33:01 +0100

    Add breadcrumbs Dusk test for divisions, clubs and teams CRUD

--- tests/Browser/BreadcrumbsTest.php
+++ tests/Browser/BreadcrumbsTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Browser;
 
 use App\Models\Competition;
+use App\Models\Division;
 use App\Models\Season;
+use App\Models\Team;
 use App\Models\User;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -47,19 +49,38 @@ class BreadcrumbsTest extends DuskTestCase
 
     public function authPagesBreadcrumbs(): array
     {
-        $competition = factory(Competition::class)->create();
-        
+        /** @var Division $division */
+        $division = factory(Division::class)->create();
+        $divisionId = $division->getId();
+        $competition = $division->getCompetition();
         $competitionId = $competition->getId();
-        $seasonId = $competition->getSeason()->getId();
-        
+        $season = $competition->getSeason();
+        $seasonId = $season->getId();
+
+        /** @var Team $team */
+        $team = aTeam()->build();
+        $teamId = $team->getId();
+        $club = $team->getClub();
+        $clubId = $club->getId();
+
         return [
             "/dashboard" => ['Home', 'Dashboard'],
-            "/seasons" => ['Home', 'Seasons'],
-            "/seasons/create" => ['Home', 'Seasons', 'New season'],
-            "/seasons/$seasonId/edit" => ['Home', 'Seasons', 'Edit season'],
-            "/seasons/$seasonId/competitions" => ['Home', 'Seasons', 'Competitions'],
-            "/seasons/$seasonId/competitions/create" => ['Home', 'Seasons', 'Competitions', 'New competition'],
-            "/seasons/$seasonId/competitions/$competitionId/edit" => ['Home', 'Seasons', 'Competitions', 'Edit competition'],
+            "/seasons" => ['Home', 'Dashboard', 'Seasons'],
+            "/seasons/create" => ['Home', 'Dashboard', 'Seasons', 'New season'],
+            "/seasons/$seasonId/edit" => ['Home', 'Dashboard', 'Seasons', 'Edit season'],
+            "/seasons/$seasonId/competitions" => ['Home', 'Dashboard', 'Seasons', 'Competitions'],
+            "/seasons/$seasonId/competitions/create" => ['Home', 'Dashboard', 'Seasons', 'Competitions', 'New competition'],
+            "/seasons/$seasonId/competitions/$competitionId/edit" => ['Home', 'Dashboard', 'Seasons', 'Competitions', 'Edit competition'],
+            "/competitions/$competitionId/divisions" => ['Home', 'Dashboard', 'Seasons', 'Competitions', 'Divisions'],
+            "/competitions/$competitionId/divisions/create" => ['Home', 'Dashboard', 'Seasons', 'Competitions', 'Divisions', 'New division'],
+            "/competitions/$competitionId/divisions/$divisionId/edit" => ['Home', 'Dashboard', 'Seasons', 'Competitions', 'Divisions', 'Edit division'],
+
+            "/clubs" => ['Home', 'Dashboard', 'Clubs'],
+            "/clubs/create" => ['Home', 'Dashboard', 'Clubs', 'New club'],
+            "/clubs/$clubId/edit" => ['Home', 'Dashboard', 'Clubs', 'Edit club'],
+            "/clubs/$clubId/teams" => ['Home', 'Dashboard', 'Clubs', 'Teams'],
+            "/clubs/$clubId/teams/create" => ['Home', 'Dashboard', 'Clubs', 'Teams', 'New team'],
+            "/clubs/$clubId/teams/$teamId/edit" => ['Home', 'Dashboard', 'Clubs', 'Teams', 'Edit team'],
         ];
     }
 }
