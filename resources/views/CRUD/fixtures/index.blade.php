1b75fe91bda86793a9c94d6ede6902e4527502d5
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2020-01-16 13:50:27 +0000

    Install Vuetify

--- resources/views/CRUD/fixtures/index.blade.php
+++ resources/views/CRUD/fixtures/index.blade.php
@@ -3,3 +3,8 @@
 @section('content')
 <div id="fixtures"></div>
 @endsection
+
+@push('stylesheets')
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <link href="{{ mix('css/vuetify.min.css') }}" rel="stylesheet">
+@endpush
