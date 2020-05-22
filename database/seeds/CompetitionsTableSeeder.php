276e2f96c6aa32baebc5167e7933db08553760d5
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2020-05-22 11:39:20 +0100

    Create Competition Admin roles for each competition seeded

--- database/seeds/CompetitionsTableSeeder.php
+++ database/seeds/CompetitionsTableSeeder.php
@@ -3,6 +3,7 @@
 use App\Models\Competition;
 use App\Models\Season;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class CompetitionsTableSeeder extends Seeder
 {
@@ -10,6 +11,15 @@ class CompetitionsTableSeeder extends Seeder
     {
         Season::each(function (Season $season): void {
             factory(Competition::class)->create(['season_id' => $season->id]);
+        });
+
+        $this->createRoles();
+    }
+
+    private function createRoles(): void
+    {
+        Competition::all()->each(function (Competition $competition) {
+            Role::create(['name' => "Competition {$competition->getId()} Admin"]);
         });
     }
 }
