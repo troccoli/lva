4e62ec9c9a03f1dd7a5a0d12e6539922badd5831
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2020-05-22 15:32:05 +0100

    Add a user for each role
    Fix #87

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Club;
+use App\Models\Competition;
+use App\Models\Division;
+use App\Models\Season;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -12,5 +17,42 @@ class UsersTableSeeder extends Seeder
         $user = factory(User::class)->create();
 
         $user->assignRole('Site Admin');
+
+        Season::all()->each(function (Season $season) {
+            $user = factory(User::class)->create([
+                'name' => "Season {$season->getName()} Admin",
+                'email' => "season-{$season->getId()}-admin@example.com",
+            ]);
+            $user->assignRole("Season {$season->getName()} Admin");
+        });
+        Competition::all()->each(function (Competition $competition) {
+            $user = factory(User::class)->create([
+                'name' => "Competition {$competition->getId()} Admin",
+                'email' => "competition-{$competition->getId()}-admin@example.com",
+            ]);
+            $user->assignRole("Competition {$competition->getId()} Admin");
+        });
+        Division::all()->each(function (Division $division) {
+            $user = factory(User::class)->create([
+                'name' => "Division {$division->getId()} Admin",
+                'email' => "division-{$division->getId()}-admin@example.com",
+            ]);
+            $user->assignRole("Division {$division->getId()} Admin");
+        });
+
+        Club::all()->each(function (Club $club) {
+            $user = factory(User::class)->create([
+                'name' => "Club {$club->getId()} Secretary",
+                'email' => "club-{$club->getId()}-secretary@example.com",
+            ]);
+            $user->assignRole("Club {$club->getId()} Secretary");
+        });
+        Team::all()->each(function (Team $team) {
+            $user = factory(User::class)->create([
+                'name' => "Team {$team->getId()} Secretary",
+                'email' => "team-{$team->getId()}-secretary@example.com",
+            ]);
+            $user->assignRole("Team {$team->getId()} Secretary");
+        });
     }
 }
