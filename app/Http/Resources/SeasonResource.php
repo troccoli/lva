1cfa5bac48da2fb6cf12e2e085624d0428359694
Giulio Troccoli-Allard <giulio@troccoli.it>
2020-06-13 12:18:08 +0100

    Add permissions to /seasons API

--- app/Http/Resources/SeasonResource.php
+++ app/Http/Resources/SeasonResource.php
@@ -4,19 +4,27 @@ namespace App\Http\Resources;
 
 use App\Http\Controllers\Api\V1\LoadRelations;
 use App\Models\Season;
-use Illuminate\Http\Request;
+use App\Repositories\AccessibleCompetitions;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SeasonResource extends JsonResource
 {
     use LoadRelations;
 
-    protected $relationsAllowed = ['competitions'];
+    protected array $relationsAllowed = ['competitions'];
+    private AccessibleCompetitions $accessibleCompetitions;
+
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+
+        $this->accessibleCompetitions = resolve(AccessibleCompetitions::class);
+    }
 
     /**
      * @param \Illuminate\Http\Request $request
      */
-
     public function toArray($request): array
     {
         $this->loadRelations($request);
@@ -25,9 +33,13 @@ class SeasonResource extends JsonResource
         $season = $this->resource;
 
         return [
-            'id'   => $season->getId(),
+            'id' => $season->getId(),
             'name' => $season->getName(),
-            'competitions' => CompetitionResource::collection($this->whenLoaded('competitions')),
+            'competitions' => CompetitionResource::collection(
+                $this->whenLoaded('competitions', function () use ($request, $season): Collection {
+                    return $this->accessibleCompetitions->inSeason($season)->get($request->user());
+                })
+            ),
         ];
     }
 }
