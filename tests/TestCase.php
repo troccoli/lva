c7540d5687f834a21162d2fb768c840029492b45
Giulio Troccoli-Allard <giulio.troccoli-allard@theshopworks.com>
2020-06-21 15:19:07 +0100

    Add useful methods for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,7 @@ use App\Helpers\RolesHelper;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -23,6 +24,18 @@ abstract class TestCase extends BaseTestCase
         Role::create(['name' => RolesHelper::SITE_ADMIN]);
         Permission::create(['name' => 'view-seasons']);
 
-        $this->siteAdmin = factory(User::class)->create()->assignRole(RolesHelper::SITE_ADMIN);
+        $this->siteAdmin = $this->userWithRole(RolesHelper::SITE_ADMIN);
+    }
+
+    protected function keyArrayBy(array $items, string $key): array
+    {
+        return Collection::make($items)
+            ->keyBy($key)
+            ->toArray();
+    }
+
+    protected function userWithRole(string $role): User
+    {
+        return factory(User::class)->create()->assignRole($role);
     }
 }
