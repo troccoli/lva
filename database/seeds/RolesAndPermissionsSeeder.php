044a1116c0b1c049faa1c3237b6d56f62881b67d
Giulio Troccoli-Allard <giulio@troccoli.it>
2020-07-26 16:59:37 +0100

    Remove unused roles

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -15,11 +15,6 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        /* todo: this permission and roles are redundant and will be removed */
-        Role::create(['name' => 'League Administrator']);
-        Role::create(['name' => 'Division Administrator']);
-        /* ------ */
-
         Permission::create(['name' => PermissionsHelper::addSeason()]);
         Role::create(['name' => RolesHelper::SITE_ADMIN]);
         Role::create(['name' => RolesHelper::REF_ADMIN]);
