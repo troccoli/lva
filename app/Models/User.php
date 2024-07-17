44bd90f8a8c720cd973a8bc5c2280b705e5afc26
Giulio Troccoli-Allard <giulio@troccoli.it>
2024-07-17 16:28:00 +0100

    Use UUID for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory,
+        HasUuids,
+        Notifiable;
 
     /**
      * The attributes that are mass assignable.
